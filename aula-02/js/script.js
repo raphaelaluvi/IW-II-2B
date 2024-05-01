16c82691c0217b6d037a8fae72824ba2492d99b8
raphaelaluvi <raphaluvi@gmail.com>
2024-05-01 19:47:31 -0300

    teste

--- aula-02/js/script.js
+++ aula-02/js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const pulo = () => {
     mario.classList.add('pulo')
@@ -9,5 +10,28 @@ const pulo = () => {
 
     }, 500)
 }
+
+const loop =  setInterval (() => {
+
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'img/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+}
+, 10)
 
 document.addEventListener('keydown', pulo)
