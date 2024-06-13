2d61f947282ff975cc82266814e68be7ed9c196e
raphaelaluvi <raphaluvi@gmail.com>
2024-06-13 09:01:47 -0300

    aula05

--- cursoNode/05_handlebars/index.js
+++ cursoNode/05_handlebars/index.js
@@ -2,8 +2,30 @@ const express = require('express')
 //instanciar uma variavel teste**
 //const pq n pode mudar dps
 const app = express()
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
+// configuracoes
 
+    // template engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    // conexao com o banco
+    const sequelize = new Sequelize('test', 'root', '', {
+        host:'localhost', 
+        dialect: 'mysql'
+    })
+
+// rotas
+app.get('/cadastro', function(req, res){
+    //res.send('rota de cadastro de posts')
+    res.render('formulario')
+})
+
+app.post('/add',function(req, res){
+    res.send('formulário enviado')
+})
 
 //cria o servidor e vai ficar de olho na porta
 app.listen(8081, function(){ //no listen n passa req e res
